5f00e3bb1ad98adb37e6250908d1d205a4fc95f0
ChipTalksComputers <57562234+ChipTalksComputers@users.noreply.github.com>
2020-02-28 08:25:01 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,9 +1,11 @@
+#Input
 print("Enter your array")
 
 array = list(map(int, input().split()))
 
 n = len(array)
 
+#Sorting
 for i in range(1,n):
     j = i
     while (j>=1):
@@ -15,6 +17,6 @@ for i in range(1,n):
 
         j-=1
 
-
+#Output
 for element in array:
     print(element, end= " ")
